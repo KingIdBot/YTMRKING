619b841c0a00d193ccbe62efaa9955bb9a0655b3
KingIdBot <77149298+KingIdBot@users.noreply.github.com>
2021-01-18 12:40:36 +0800

    Add files via upload

---
+++ plugins/help.js
@@ -0,0 +1,23 @@
+exports.help = (id, TanpaNama tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+*🔰 MR ᭄KINGツ 🔰*
+
+❉──────────❉
+
+📆 *${tampilTanggal}*
+📌STATUS BOT AKTIF: *wa.me/+1831353-5216*
+	
+╔════════════════════
+║ *ABOUT  MR ᭄KINGツ*         
+╠════════════════════
+║╭──❉ *DAFTAR* ❉──
+║│➸ _*#menu1*_
+║│➸ _*#menu2*_
+║│➸ _*#donate*_
+║│➸ _*#info*_
+║╰───────────
+╠════════════════════
+╚════════════════════`
+}
---
+++ plugins/menu1.js
@@ -0,0 +1,67 @@
+exports.menu1 = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+*🔰MR ᭄KINGツ🔰*
+
+❉──────────❉
+
+📆 *${tampilTanggal}*
+📌STATUS BOT AKTIF: *wa.me/+1831353-5216*
+
+╔════════════════════
+║ *MENU1 MR ᭄KINGツ*
+╠════════════════════
+║╭──❉ *MEDIA* ❉──
+║│➸ _*!sticker*_
+║│➸ _*!foto cewek*_
+║│➸ _*!foto cowok*_
+║│➸ _*!pokemon*_ 
+║│➸ _*!loli*_
+║│➸ _*!fotoanime*_
+║│➸ _*!waifu*_
+║│➸ _*!ttp*_ <teks>
+║│➸ _*!meme*_
+║│➸ _*!ssweb*_ <link>
+║╰───────────
+╠════════════════════
+║╭──❉ *EDUCATION* ❉──
+║│➸ _*!nulis*_ <teks>
+║│➸ _*!pantun*_
+║│➸ _*!artinama*_ <nama anda>
+║│➸ _*!pasangan*_ <Aris & Cahya>
+║│➸ _*!lirik*_ <nama lagu>
+║│➸ _*!chord*_ <nama lagu>
+║│➸ _*!bucin*_
+║│➸ _*!zodiak*_ <28-02-2002>
+║│➸ _*#*_  <teks>
+║│➸ _*!cooltext*_ <teks>
+║╰───────────
+╠════════════════════
+║╭──❉ *INFORMATION* ❉──
+║│➸ _*!sholat*_ <daerah>
+║│➸ _*!covidID*_
+║│➸ _*!covidcountry*_ <negara>
+║│➸ _*!infogempa*_ 
+║│➸ _*!cektanggal*_ <Tanggal>
+║│➸ _*!infoanime*_ <Naruto>
+║│➸ _*!filmanime*_ <Naruto>
+║│➸ _*!wiki*_ <Buaya>
+║│➸ _*!wikien*_ <dog>
+║│➸ _*!resep*_ <nasi bakar>
+║│➸ _*!jadwalTVnow*_ <channel>
+║╰───────────
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│➸ *Youtube*
+║│ https://youtube.com/channel/UCHK9gIokua2zkYKYTbqAwMQ
+║│➸ *Creator MR ᭄KINGツ*
+║╰───────────
+║      *🔰MR ᭄KINGツ🔰*
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║      *🔰MR ᭄KINGツ🔰*
+╠════════════════════
+║ _*POWERED BY MR ᭄KINGツ*
+╚════════════════════`
+}
